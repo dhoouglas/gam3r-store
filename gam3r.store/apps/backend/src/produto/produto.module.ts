e79551260bc56f8a501756543beb25cf42547eb2
dhoouglas <dhoouglas@hotmail.com>
2024-10-02 15:24:04 -0300

    Configured Prisma, connection and seed with the database and created Produto table

--- gam3r.store/apps/backend/src/produto/produto.module.ts
+++ gam3r.store/apps/backend/src/produto/produto.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { ProdutoController } from './produto.controller';
 
-@Module({})
+@Module({
+  controllers: [ProdutoController]
+})
 export class ProdutoModule {}
